65b68e224798a94702e48811b9acf228822afb9b
Leonardo Moreno <leon@mac.local>
2021-10-04 20:42:27 -0400

    test chez moi

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,14 +12,14 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	int	cont_1;
 	int	cont_2;
 	int	cont_3;
 
 	cont_1 = 0;
-	while (haystack[cont_1] != '\0')
+	while (haystack[cont_1] != '\0' && cont_1 < (int) len)
 	{
 		cont_2 = 0;
 		if (haystack[cont_1] == needle[cont_2])
--- libft.h
+++ libft.h
@@ -23,7 +23,7 @@ char *ft_strrchr(const char *s, int c, int **ptr);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
-char *ft_strnstr(const char *haystack, const char *needle);
+char *ft_strnstr(const char *haystack, const char *needle, size_t len);
 int ft_atoi(const char *str);
 void *ft_calloc(size_t count, size_t size);
 char *ft_strdup(const char *s1);
