0c5c3dcfa2bb6ea3315112fc5b46ef3d30a05f8a
Leonardo Moreno <lmoreno@c1r8p7.42quebec.com>
2021-10-28 13:01:35 -0400

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: LeoMoreno <lmoreno@student.42quebec.>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 13:21:45 by LeoMoreno         #+#    #+#             */
-/*   Updated: 2021/10/01 18:38:43 by LeoMoreno        ###   ########.fr       */
+/*   Updated: 2021/10/23 18:04:47 by LeoMoreno        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			cont;
-	unsigned char	*ptr;
 
 	cont = 0;
-	ptr = b;
 	while (cont < len)
 	{
-		ptr[cont] = (unsigned char)c;
+		((unsigned char *)b)[cont] = (unsigned char)c;
 		cont++;
 	}
 	return (b);
