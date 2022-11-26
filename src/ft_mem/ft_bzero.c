e6d654a534e4f6c4c9790f8e86153d74277f1a06
Leo <leon.moreno@pm.me>
2022-11-26 13:40:15 -0500

    update

--- src/ft_mem/ft_bzero.c
+++ src/ft_mem/ft_bzero.c
@@ -3,18 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: LeoMoreno <lmoreno@student.42quebec.com    +#+  +:+       +#+        */
+/*   By: lmoreno <lmoreno@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/01 14:17:38 by LeoMoreno         #+#    #+#             */
-/*   Updated: 2021/11/01 14:25:13 by LeoMoreno        ###   ########.fr       */
+/*   Created: 2022/10/26 15:03:14 by lmoreno           #+#    #+#             */
+/*   Updated: 2022/10/26 15:03:15 by lmoreno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+	/*size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)s)[i] = 0;
+		i++;
+	}*/
+void	*ft_bzero(void *s, size_t n)
 {
-	if (n == 0)
-		return ;
-	ft_memset(s, 0, n);
+	return (ft_memset(s, 0, n));
 }
